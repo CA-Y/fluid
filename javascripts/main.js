50810fbd63dc165b953b2708d2db523945ecdb30
Mansour <mansour@oxplot.com>
2012-04-12 13:02:32 +1000

    As there can be other guides, made welldrop one more speicific

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,7 @@
 (function (window) {
   $(function(){
-    var showGuide = $('#show-guide');
-    var guide = $('#guide');
+    var showGuide = $('#show-welldrop-guide');
+    var guide = $('#welldrop-guide');
     var clickHandler = function(){
       var visible = guide.css('display') == 'block';
       if (visible)
@@ -10,11 +10,11 @@
         guide.css('display', 'block')
       showGuide.text(!visible ? 'Hide Guide' : 'Show Guide');
       if (!visible)
-        window.location.hash = '#guide';
+        window.location.hash = '#welldrop-guide';
       return false;
     };
     showGuide.click(clickHandler);
-    if (window.location.hash == '#guide')
+    if (window.location.hash == '#welldrop-guide')
       clickHandler();
   });
 })(window)
